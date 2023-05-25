eefe5d31c49f1822f169c82bbdb1e5244b123041
Natalie Campbell <124435060+NatalieCampbellGit@users.noreply.github.com>
2023-05-25 22:38:45 +1000

    adding the raw logo code

---
+++ examples/logo.js
@@ -0,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200"><svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+      <rect x="150" y="75" width="250" height="250" fill="Purple" />
+      <text x="225" y="150" dominant-baseline="middle" text-anchor="middle" fill="Yellow" font-size="60">Nat</text>
+    </svg></svg>
